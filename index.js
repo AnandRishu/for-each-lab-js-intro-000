ea95440903c089501ac1ac00ef10cdb97971d562
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-12-13 17:52:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(arr) {
+  return arr.forEach(function (element, index, array) {
+    console.log(`${index}: ${element}`);
+  });
+  
+}
+
+function iterate(iterativeLog) {
+  var arr = [1, 2, 3, 4];
+  arr.forEach(iterativeLog)
+  return arr;
+}
+
+function doTOArray(arr, terativeLog) {
+  arr.forEach(callback);
+}
